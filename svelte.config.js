520b07953162be2c11c98e7a10a18c73fbc18bbe
ZennoZenith <ZennoZenith@proton.me>
2024-10-31 18:46:16 +0530

    feat: runes true

--- svelte.config.js
+++ svelte.config.js
@@ -7,15 +7,16 @@ const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: [vitePreprocess(), mdsvex()],
-
   kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
   },
-
   extensions: [".svelte", ".svx"],
+  compilerOptions: {
+    runes: true,
+  },
 };
 
 export default config;
